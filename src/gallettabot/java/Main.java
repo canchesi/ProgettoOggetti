bdcf0ad3d37e1c4982e308babffc9ad8c30066c8
Claudio Anchesi <claudio.anchesi@outlook.it>
2022-08-09 16:47:23 +0200

    Potrei arrabbiarmi.

--- src/gallettabot/java/Main.java
+++ src/gallettabot/java/Main.java
@@ -1,6 +1,9 @@
 package src.gallettabot.java;
 
 import com.mongodb.*;
+import com.mongodb.MongoClient;
+import com.mongodb.client.*;
+import org.bson.Document;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -10,15 +13,15 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            /*
-            * MongoClientURI URI = new MongoClientURI("mongodb://93.40.7.204:27017");
-            * MongoClient mongo = new MongoClient(URI);
-            * DB database = mongo.getDB("test");
-            * DBCollection collection = database.getCollection("prova");
-            * List<Integer> books = Arrays.asList(27464, 747854);
-            * DBObject person = new BasicDBObject("_id", "jo").append("books", books);
-            * collection.insert(person);
-            * */
+
+             MongoClientURI URI = new MongoClientURI("mongodb://93.40.7.204:27017");
+             MongoClient mongo = new MongoClient(URI);
+             MongoDatabase database = mongo.getDatabase("test");
+             MongoCollection<Document> collection = database.getCollection("prova");
+             List<Integer> books = Arrays.asList(27464, 747854);
+             Document person = new Document("_id", "jo").append("books", books);
+             collection.insertOne(person);
+
             TelegramBotsApi telegram = new TelegramBotsApi(DefaultBotSession.class);
             telegram.registerBot(new GallettaBot());
         } catch (TelegramApiException e) {
