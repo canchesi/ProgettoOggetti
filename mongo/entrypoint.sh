71483484e5f94626118fe091eac067390b804e76
Claudio Anchesi <claudio.anchesi@outlook.it>
2022-09-19 01:07:04 +0200

    Sistemazione entrypoint.sh

--- mongo/entrypoint.sh
+++ mongo/entrypoint.sh
@@ -4,10 +4,5 @@ check=$(mongosh /docker-entrypoint-initdb.d/scripts/checkInit.js);
 if [ ${check: -1} == "0" ]; then
     
     /docker-entrypoint-initdb.d/scripts/init.sh
-    echo "NON C'È NIENTE\n\n\n\n\n\n\n\n\n\n";
-
-else
-
-    echo "C'È QUALCOSA\n\n\n\n\n\n\n\n\n\n"
 
 fi
